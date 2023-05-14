0d3e0753fc4416da5225d89a731eb9f8b8dd7d4b
Roman_Marchukov <romamarchukov@icloud.com>
2023-05-14 16:44:10 +0300

    some add

--- tel_bot.py
+++ tel_bot.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 
-from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
 import requests
@@ -46,23 +46,22 @@ async def delete_command_handler(message: types.Message):
 
 @dp.message_handler(commands=['all_services'])
 async def services_handler(message: types.Message):
-    services_keyboard = ReplyKeyboardMarkup()
+    services_keyboard = InlineKeyboardMarkup()
     text = requests.get('http://127.0.0.1:8000/services')
     my = text.json()
     all_services = my['services']['services']
     for i in all_services:
-        services_keyboard.insert(i['name'])
-    await message.reply(f"services {message}", reply_markup=services_keyboard)
+        services_keyboard.insert(InlineKeyboardButton(text=i['name'], callback_data='some info'))
+    await message.reply('Choose your service!', reply_markup=services_keyboard)
 
-
-@dp.message_handler(commands=['service'])
-async def service_handler(message: types.Message):
-    info = message.text.split(' ')[1]
-    text = requests.get('http://127.0.0.1:8000/services')
-    my = text.json()
-    message.text = my['services']['services'][int(info)]
-    a = message.text
-    await message.reply(a)
+# services = []
+# response = requests.get('http://127.0.0.1:8000/services')
+# g = response.json()
+# all_services = g['services']['services']
+# count = 0
+# for i in all_services:
+#     services.append({'number': count, 'name': i['name'], 'id': i['id']})
+#     count += 1
 
 
 @dp.message_handler(commands=['start'])
@@ -82,19 +81,32 @@ async def take_message_info(message: types.Message):
     message_id = message.message_id
     chat_id = message.chat.id
     text = message.text
-    return {'message_id': message_id, 'chat_id': chat_id, 'text': text}
+    services = {}
+    service = requests.get('http://127.0.0.1:8000/services')
+    my = service.json()
+    all_services = my['services']['services']
+    for i in all_services:
+        services.update({i['name']: i['id']})
+    question = ''
+    try:
+        info = requests.get(f'http://127.0.0.1:8000/services/{services[text]}?text={question}')
+        a = info.json()
+        final = a['service']['messages'][0]['text']
+        return {'message_id': message_id, 'chat_id': chat_id, 'text': text, 'final': final}
+    except KeyError:
+        final = text
+        return {'message_id': message_id, 'chat_id': chat_id, 'text': text, 'final': final}
 
 
 @dp.message_handler(take_message_info)
-async def my_message_handler(message: types.Message, text, chat_id, message_id):
+async def my_message_handler(message: types.Message, text, chat_id, message_id, final):
     message.message_id = message_id
     message.chat.id = chat_id
-    message.text = text
-    all_services = requests.get('http://127.0.0.1:8000/services')
-    my = all_services.json()
-    for i in my['services']['services']:
-        if i['name'] == text:
-            await message.reply(text)
+    if text != final:
+        message.text = final
+    elif text == final:
+        message.text = 'There is not this service'
+    await message.answer(message.text)
 
 
 if __name__ == '__main__':
--- test_fastAPI.py
+++ test_fastAPI.py
@@ -1,59 +1,32 @@
-from typing import Union
 import requests
-from fastapi import FastAPI, Header
+from fastapi import FastAPI
 import json
 
 app = FastAPI()
 
 
 @app.get("/services")
-async def get_services(key: str = Header('')):
-    if key is None:
-        return {"error": "API key is missing"}
+async def get_services():
 
     url = "https://tonai.tech/api/public/v1/services"
     headers = {"key": 'Smoove_ff5acd56-d794-44ae-ad87-9a9b02705890'}
 
     response = requests.get(url, headers=headers)
 
-    if response.status_code != 200:
-        return {"error": "Failed to retrieve services"}
-
     services = response.json()
 
     return {"services": services}
 
 
-# @app.get("/services/{name}")
-# async def get_service(name: str):
-#     if name is None:
-#         return {"error": "API key is missing"}
-#
-#     url = "https://tonai.tech/api/public/v1/services"
-#     headers = {"key": 'Smoove_ff5acd56-d794-44ae-ad87-9a9b02705890'}
-#
-#     response = requests.get(url, headers=headers)
-#
-#     if response.status_code != 200:
-#         return {"error": "Failed to retrieve services"}
-#
-#     name = response.json()
-#
-#     return {"name": name}
-
-
-@app.get("/services/{name}")
-async def get_service(name: str):
-    if name is None:
-        return {"error": "API key is missing"}
-
-    url = "https://dev.tonai.tech/api/public/v1/services"
-    payload = {
-        'service_id': "qdfmo6h8dn62wyx",
-        'input_text': 'Ask some question'
-    }
+@app.get("/services/{service_id}")
+def read_item(service_id: str, text: str):
+    url = "https://tonai.tech/api/public/v1/services"
     headers = {"key": 'Smoove_ff5acd56-d794-44ae-ad87-9a9b02705890'}
 
+    payload = {
+        'service_id': service_id,
+        'input_text': text
+    }
+
     response = json.loads(requests.post(url, json=payload, headers=headers).text)
-    print(response)
-    return response
+    return {"service": response}
