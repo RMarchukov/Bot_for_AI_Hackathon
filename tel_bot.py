659973dc5befd1ce7b760e88305396b4ded19eb4
Roman_Marchukov <romamarchukov@icloud.com>
2023-05-17 14:58:58 +0300

    worked bot

--- tel_bot.py
+++ tel_bot.py
@@ -1,29 +1,33 @@
-import logging
 import os
+import requests
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
-import requests
+
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-@dp.message_handler(commands=['add'])
-async def set_commands(message: types.Message):
-    command = types.BotCommand(command=message.text.split(' ')[1], description=message.text.split(' ')[2])
-    commands = await bot.get_my_commands()
-    commands.append(command)
-    await message.reply(f'Команда {command.command} додана до меню бота')
-    await bot.set_my_commands(commands)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f"Hi {name}! I'm bot for tonAI!")
+    await services_handler(message)
 
+# @dp.message_handler(commands=['add'])
+# async def set_commands(message: types.Message):
+#     command = types.BotCommand(command=message.text.split(' ')[1], description=message.text.split(' ')[2])
+#     commands = await bot.get_my_commands()
+#     commands.append(command)
+#     await message.reply(f'Команда {command.command} додана до меню бота')
+#     await bot.set_my_commands(commands)
 
 @dp.message_handler(commands=['help'])
 async def show_commands(message: types.Message):
@@ -36,18 +40,19 @@ async def show_commands(message: types.Message):
         await message.answer('None')
 
 
-@dp.message_handler(commands=['del'])
-async def delete_command_handler(message: types.Message):
-    command_name = message.text.split(' ')[1]
-    commands = await bot.get_my_commands()
-    commands = [command for command in commands if command.command != command_name]
-    await bot.set_my_commands(commands)
-    await message.answer(f'Команда {command_name} видалена з меню бота')
+# @dp.message_handler(commands=['del'])
+# async def delete_command_handler(message: types.Message):
+#     command_name = message.text.split(' ')[1]
+#     commands = await bot.get_my_commands()
+#     commands = [command for command in commands if command.command != command_name]
+#     await bot.set_my_commands(commands)
+#     await message.answer(f'Команда {command_name} видалена з меню бота')
 
 
 class Form(StatesGroup):
     service = State()
     text = State()
+    params = State()
 
 
 async def get_data():
@@ -83,11 +88,6 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     await message.answer('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
 
 
-# @dp.message_handler(lambda message: message.text not in ["Male", "Female", "Other"], state=Form.service)
-# async def process_gender_invalid(message: types.Message):
-#     return await message.reply("Bad gender name. Choose your gender from the keyboard.")
-
-
 @dp.message_handler(state=Form.service)
 async def process_name(message: types.Message, state: FSMContext):
     number = 0
@@ -102,18 +102,31 @@ async def process_name(message: types.Message, state: FSMContext):
     g = response.json()
     list_of_params = []
     count = 0
+    if g['services']['services'][number]['params']['required'] is None:
+        print('There are not any main parameters.')
+
     for i in g['services']['services'][number]['params']['required']:
-        print(i)
-        list_of_params.append(i['name'])
+        list_of_params.append(f'{i["name"]}_main')
         count += 1
     if message.text not in g['services']['services'][number]['name']:
-        return await message.answer("Service not found")
+        return await message.answer("There is not this service. Choose service from buttons.")
     else:
-        async with state.proxy() as data:
-            data['service'] = message.text
-        markup = types.ReplyKeyboardMarkup().insert(types.KeyboardButton('/cancel'))
-        await Form.next()
-        await message.answer(f"Your question: {list_of_params}", reply_markup=markup)
+        if g['services']['services'][number]['params'] is not None:
+            if g['services']['services'][number]['params']['optional'] is None:
+                print('There are not any optional parameters.')
+            else:
+                count = 0
+                for i in g['services']['services'][number]['params']['optional']:
+                    list_of_params.append(f"{i['name']}_optional")
+                    count += 1
+
+    async with state.proxy() as data:
+        data['service'] = message.text
+        data['params'] = list_of_params
+    markup = types.ReplyKeyboardMarkup().insert(types.KeyboardButton('/cancel'))
+    await Form.next()
+    param = ', ~'.join(list_of_params)
+    await message.answer(f"Enter your params. Use comma and space between parameters: ~{param}", reply_markup=markup)
 
 
 @dp.message_handler(state=Form.text)
@@ -125,26 +138,36 @@ async def process_gender(message: types.Message, state: FSMContext):
             for i, b in v.items():
                 if i == data['service']:
                     my_id = b
-        markup = types.ReplyKeyboardRemove()
-        info = requests.get(f'http://127.0.0.1:8000/services/{my_id}?text={data["text"]}')
-        # a = info.json()
-        # final = a['service']['messages'][0]['text']
-        await message.answer(f"{data['service']}", reply_markup=markup)
-    await state.finish()
 
+        markup = types.ReplyKeyboardRemove()
+        result = data['text'].split(', ')
+        count = 1
+        a = {}
+        for i in result:
+            a.update({f'{count}': i})
+            count += 1
+
+        payload = {'service_id': my_id}
+        count = 1
+        for i in data['params']:
+            if i[-1] == 'n':
+                u = i[0:-5]
+                payload[f'{u}'] = a.get(f'{count}')
+            else:
+                u = i[0:-9]
+                payload[f'{u}'] = a.get(f'{count}')
+            count += 1
+
+        info = requests.post(f'http://127.0.0.1:8000/services/{my_id}', json=payload)
+        a = info.json()
+        await message.answer(f"{a}", reply_markup=markup)
+    await state.finish()
 
 # @dp.callback_query_handler(lambda callback: True)
 # async def choice_market_callback(callback: types.CallbackQuery):
 #     data = callback.data
 #     chat_id = callback.message.chat.id
 #     await bot.send_message(chat_id=chat_id, text=data)
-
-
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f"Hi {name}! I'm bot for tonAI!")
-
 
 # @dp.message_handler(lambda message: message.text == 'foo')
 # async def send_response(message: types.Message):
@@ -153,36 +176,15 @@ async def send_welcome(message: types.Message):
 #     print(message)
 #     await message.answer(message.text)
 
-
-# async def take_message_info(message: types.Message):
-#     message_id = message.message_id
-#     chat_id = message.chat.id
-#     text = message.text
-#     services = {}
-#     service = requests.get('http://127.0.0.1:8000/services')
-#     my = service.json()
-#     all_services = my['services']['services']
-#     for i in all_services:
-#         services.update({i['name']: i['id']})
-#     question = ''
-#     try:
-#         info = requests.get(f'http://127.0.0.1:8000/services/{services[text]}?text={question}')
-#         a = info.json()
-#         final = a['service']['messages'][0]['text']
-#         return {'message_id': message_id, 'chat_id': chat_id, 'text': text, 'final': final}
-#     except KeyError:
-#         return {'message_id': message_id, 'chat_id': chat_id, 'text': text, 'final': final}
+async def take_message_info(message: types.Message):
+    text = message.text
+    return {'text': text}
 
 
-# @dp.message_handler(take_message_info)
-# async def my_message_handler(message: types.Message, text, final):
-#     print(text)
-#     print(final)
-#     if text != final:
-#         message.text = final
-#     elif text == final:
-#         message.text = 'There is not this service'
-#     await message.answer(message.text)
+@dp.message_handler(take_message_info)
+async def my_message_handler(message: types.Message, text):
+    message.text = f"{text} it is your message. You can use command: /all_services for use this bot."
+    await message.answer(message.text)
 
 
 if __name__ == '__main__':
--- test_fastAPI.py
+++ test_fastAPI.py
@@ -1,47 +1,26 @@
 import requests
 from fastapi import FastAPI
-import json
+from dotenv import load_dotenv
+import os
+
 
 app = FastAPI()
+load_dotenv()
+URL = os.getenv('URL')
 
 
 @app.get("/services")
 async def get_services():
 
-    url = "https://tonai.tech/api/public/v1/services"
-    headers = {"key": 'Smoove_ff5acd56-d794-44ae-ad87-9a9b02705890'}
-
-    response = requests.get(url, headers=headers)
-
+    headers = {"key": 'bot_9e7f010f-657d-4e4d-b4b8-0f5db12106ec'}
+    response = requests.get(URL, headers=headers)
     services = response.json()
-
     return {"services": services}
 
 
-@app.get("/services/{service_id}")
-def read_item(service_id: str, text: str, optional1: str = None, optional2: str = None, optional3: str = None):
-    url = "https://tonai.tech/api/public/v1/services"
-    headers = {"key": 'Smoove_ff5acd56-d794-44ae-ad87-9a9b02705890'}
+@app.post("/services/{service_id}")
+def read_item(payload: dict):
 
-    # payload = {'service_id': service_id}
-    # if service_id == "":
-    #     payload['input_text'] = text
-    # elif service_id == "z3vvmcbnwdynx2x":
-    #     payload['request'] = text
-    # elif service_id == "gas59jwbg2sjtrj":
-    #     payload['job_title'] = text
-    #     payload['skill_set'] = optional1
-    #     payload['language'] = optional2
-    #     payload['additional_prompt'] = optional3
-    # elif service_id == "g9g32xxqbhyjojz":
-    #     payload['name'] = text
-    #     payload['language'] = optional1
-    #     payload['additional_prompt'] = optional2
-    # elif service_id == "if6vectzvq6tr74":
-    #     payload['topic'] = text
-    #     payload['language'] = optional1
-    #     payload['additional_prompt'] = optional2
-
-
-    response = json.loads(requests.post(url, json=payload, headers=headers).text)
+    headers = {"key": 'bot_9e7f010f-657d-4e4d-b4b8-0f5db12106ec'}
+    response = requests.post(URL, json=payload, headers=headers).json()
     return {"service": response}
